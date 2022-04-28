3ef632436588e899e076b0fe8128c169df83d1b0
codersait <codersait@gmail.com>
2022-04-28 17:29:12 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/*----Packages--*/
+import input from 'prompt-sync';
+const prompt = input();
+/*----Modules--*/
+
+/* clear the console */
+console.clear()
+
+/* Start to code here*/
+
+const name = prompt('What is your name? ');
+console.log(`Hello ${name}`);
